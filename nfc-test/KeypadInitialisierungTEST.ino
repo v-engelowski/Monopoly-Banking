a59a626afb28febc16dcbfd9d303ae3b74e94dc3
Hydra-49 <65465277+Hydra-49@users.noreply.github.com>
2023-01-16 09:08:09 +0100

    Add files via upload
    - changed Keypad
    -

--- nfc-test/KeypadInitialisierungTEST.ino
+++ nfc-test/KeypadInitialisierungTEST.ino
@@ -7,10 +7,10 @@ const byte COL_PINS[COL] = { 5, 4, 3, 2 };
 const byte ROW_PINS[ROW] = { 6, 7, 8, 9 };
 
 const char KEYS[ROW][COL] = {
-  { '1', '4', '7', '*' },
-  { '2', '5', '8', '0' },
-  { '3', '6', '9', '#' },
-  { 'A', 'B', 'C', 'D' }
+  { '*', '0', '#', 'D' },
+  { '7', '8', '9', 'C' },
+  { '1', '2', '3', 'A' },
+  { '4', '5', '6', 'B' }
 };
 
 Keypad keypad = Keypad(makeKeymap(KEYS), ROW_PINS, COL_PINS, ROW, COL);
